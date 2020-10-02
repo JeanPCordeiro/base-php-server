0c9cd4f5ba10bb43b532478a30ec1fcae300d683
JeanPCordeiro <62947268+JeanPCordeiro@users.noreply.github.com>
2020-10-02 10:18:40 +0200

    Create init-laravel.sh

---
+++ init-laravel.sh
@@ -0,0 +1,9 @@
+cd
+composer create-project laravel/laravel mapareports
+cd mapareports
+composer require jeroennoten/laravel-adminlte
+composer require laravel/ui
+php artisan ui vue --auth
+npm install && npm run dev
+php artisan adminlte:install --force --type=full
+php artisan adminlte:plugins install
